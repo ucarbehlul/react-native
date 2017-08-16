1a5aca42cf48353216de4956c12ca9c3549200c3
Faraz Sherwani <faraz@ipsy.com>
2017-08-16 14:38:29 -0400

    Update AlertDialog import

--- ReactAndroid/src/main/java/com/facebook/react/modules/dialog/AlertFragment.java
+++ ReactAndroid/src/main/java/com/facebook/react/modules/dialog/AlertFragment.java
@@ -10,7 +10,6 @@ package com.facebook.react.modules.dialog;
 import javax.annotation.Nullable;
 
 import android.annotation.SuppressLint;
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
